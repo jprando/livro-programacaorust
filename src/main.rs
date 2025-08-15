63d6a003389f3d700dff2d23a1ef12a8811218e6
Jeudi Prando <jeudiprando@gmail.com>
2025-08-15 00:26:07 -0300

    chore(rename): gcd -> maior_divisor_comum

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(app_data.clone())
             .service(get_index)
-            .service(post_gcd)
+            .service(post_maior_divisor_comum)
     };
 
     println!("[teste-web] disponivel em http://localhost:3000");
@@ -22,7 +22,7 @@ async fn main() -> std::io::Result<()> {
 }
 
 #[derive(Deserialize)]
-struct GcdParameters {
+struct MaiorDivisorComumParametros {
     numero1: u64,
     numero2: u64,
 }
@@ -37,7 +37,7 @@ async fn get_index(request_id: Data<Arc<Mutex<u64>>>) -> impl Responder {
     HttpResponse::Ok().content_type("text/html").body(
         r#"
             <title> MDC calculadora </title>
-            <form action="/gcd" method="post">
+            <form action="/maior_divisor_comum" method="post">
                 <input type="text" name="numero1" placeholder="informe um numero" />
                 <input type="text" name="numero2" placeholder="informe um numero" />
                 <button type="submit"> calcular MDC </button>
@@ -46,25 +46,25 @@ async fn get_index(request_id: Data<Arc<Mutex<u64>>>) -> impl Responder {
     )
 }
 
-#[post("/gcd")]
-async fn post_gcd(
+#[post("/maior_divisor_comum")]
+async fn post_maior_divisor_comum(
     request_id: Data<Arc<Mutex<u64>>>,
-    form: web::Form<GcdParameters>,
+    form: web::Form<MaiorDivisorComumParametros>,
 ) -> impl Responder {
     {
         let mut id = request_id.lock().unwrap();
         *id += 1;
-        println!("[{}] 200 POST /gcd ", id);
+        println!("[{}] 200 POST /maior_divisor_comum ", id);
     }
     HttpResponse::Ok().content_type("text/html").body(format!(
         "O maior divisor comum do numero {} e {} eh <b>{}</b>",
         form.numero1,
         form.numero2,
-        gcd(form.numero1, form.numero2)
+        maior_divisor_comum(form.numero1, form.numero2)
     ))
 }
 
-fn gcd(mut numero1: u64, mut numero2: u64) -> u64 {
+fn maior_divisor_comum(mut numero1: u64, mut numero2: u64) -> u64 {
     assert!(numero1 != 0 && numero2 != 0);
 
     while numero2 != 0 {
@@ -80,8 +80,8 @@ fn gcd(mut numero1: u64, mut numero2: u64) -> u64 {
 }
 
 #[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
+fn test_maior_divisor_comum() {
+    assert_eq!(maior_divisor_comum(14, 15), 1);
 
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
+    assert_eq!(maior_divisor_comum(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
 }
